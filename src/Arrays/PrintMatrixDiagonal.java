c7b0e4958f15733fbdaae7127d857778604c0644
sethu <sethu.store@gmail.com>
2017-11-28 11:47:47 +0530

    sample input and outputs

--- src/Arrays/PrintMatrixDiagonal.java
+++ src/Arrays/PrintMatrixDiagonal.java
@@ -3,6 +3,26 @@ package Arrays;
 public class PrintMatrixDiagonal {
 
 
+    /*
+
+    input
+    1  2  3  4  5
+    6  7  8  9  10
+    11 12 13 14 15
+    16 17 18 19 20
+
+
+    output
+             1
+             6 2
+             11 7 3
+             16 12 8 4
+             17 13 9 5
+             18 14 10
+             19 15
+             20
+
+    */
     public static void main(String[] args) {
         int[][] a=new int[4][5];
         a[0][0]=1;a[0][1]=2;a[0][2]=3;a[0][3]=4;a[0][4]=5;
